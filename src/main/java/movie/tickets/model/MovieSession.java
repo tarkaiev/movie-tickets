241f527ab358b14b7f7401acffc9d9bbddfe6d22
tarkaiev <tarkaievd@gmail.com>
2020-10-06 14:08:07 +0300

    implemented models

---
+++ src/main/java/movie/tickets/model/MovieSession.java
@@ -0,0 +1,61 @@
+package movie.tickets.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "movie_sessions")
+public class MovieSession {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Movie move;
+    @ManyToOne
+    private CinemaHall cinemaHall;
+    private LocalDateTime showTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Movie getMove() {
+        return move;
+    }
+
+    public void setMove(Movie move) {
+        this.move = move;
+    }
+
+    public CinemaHall getCinemaHall() {
+        return cinemaHall;
+    }
+
+    public void setCinemaHall(CinemaHall cinemaHall) {
+        this.cinemaHall = cinemaHall;
+    }
+
+    public LocalDateTime getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(LocalDateTime showTime) {
+        this.showTime = showTime;
+    }
+
+    @Override
+    public String toString() {
+        return "MovieSession{" + "id=" + id + ", move=" + move
+                + ", cinemaHall=" + cinemaHall
+                + ", showTime=" + showTime + '}';
+    }
+}
