3d5c004c1d8e69995738d547cd2130e1686b514c
tarkaiev <tarkaievd@gmail.com>
2020-10-22 22:28:39 +0300

    implemented controllers

--- src/main/java/movie/tickets/controller/AuthenticationController.java
+++ src/main/java/movie/tickets/controller/AuthenticationController.java
@@ -1,4 +1,21 @@
 package movie.tickets.controller;
 
+import movie.tickets.model.dto.UserRequestDto;
+import movie.tickets.security.AuthenticationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public void register(@RequestBody UserRequestDto dto) {
+        authenticationService.register(dto.getEmail(), dto.getPassword());
+    }
 }
--- src/main/java/movie/tickets/controller/MovieController.java
+++ src/main/java/movie/tickets/controller/MovieController.java
@@ -1,4 +1,37 @@
 package movie.tickets.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import movie.tickets.model.dto.MovieRequestDto;
+import movie.tickets.model.dto.MovieResponseDto;
+import movie.tickets.model.dto.mapper.MovieDtoMapper;
+import movie.tickets.service.MovieService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
 public class MovieController {
+    private final MovieService movieService;
+    private final MovieDtoMapper movieDtoMapper;
+
+    public MovieController(MovieService movieService, MovieDtoMapper movieDtoMapper) {
+        this.movieService = movieService;
+        this.movieDtoMapper = movieDtoMapper;
+    }
+
+    @PostMapping
+    public void addMovie(@RequestBody MovieRequestDto dto) {
+        movieService.add(movieDtoMapper.fromRequestDto(dto));
+    }
+
+    @GetMapping
+    public List<MovieResponseDto> getAllMovies() {
+        return movieService.getAll().stream()
+                .map(movieDtoMapper::toResponseDto)
+                .collect(Collectors.toList());
+    }
 }
